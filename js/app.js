02826b232cd44f005e7ff155b1694d03b854aa22
Andreja <andreja.prpic@gmail.com>
2019-08-26 17:05:44 +0200

    feat: render Lives and Score

--- js/app.js
+++ js/app.js
@@ -41,10 +41,13 @@ class Player {
 	constructor(sprite = 'images/char-boy.png') {
 		this.col = 2;
 		this.row = 5;
+		this.rowHeight = 83;
+		this.colWidth = 101;
 		this.sprite = sprite;
 		this.collision = false;
 		this.score = 0;
 		this.lives = 5;
+		this.reset();
 	}
 
 	//when the player reaches the water it returns to its inital position
@@ -55,6 +58,12 @@ class Player {
 		}
 	}
 	
+	reset() {
+		this.curLives = this.lives;
+		this.score = 0;
+		this.resetPosition();
+	}
+	
 	resetPosition() {
 		this.col = 2;
 		this.row = 5;
@@ -62,7 +71,20 @@ class Player {
 	}
 	
 	render() {
+		this.renderLivesAndScore();
 		ctx.drawImage(Resources.get(this.sprite), this.col * colWidth, this.row * rowHeight - 10);
+		ctx.font = '30px Charcoal, sans-serif';
+		ctx.textAlign = 'center';
+		ctx.fillStyle = '#566ED7';
+	}
+	
+	renderLivesAndScore() {
+		ctx.font = '20px Charcoal';
+		ctx.textAlign = 'left';
+		ctx.fillStyle = 'white';
+		ctx.fillText('Lives: ' + this.curLives, 10, (7 * this.rowHeight));
+		ctx.textAlign = 'right';
+		ctx.fillText('Score: ' + this.score, (5 * this.colWidth ) - 10, (7 * this.rowHeight));
 	}
 
 	handleInput(key) {
@@ -111,5 +133,6 @@ function checkCollision(enemy) {
 
 function resetCollision() {
 	player.resetPosition();
+	player.curLives--;
 	allEnemies.forEach(enemy => enemy.collision = false);
 }
