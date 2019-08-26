dd3a6a1bb5ace43a71b8595b095fec32773f9af4
Andreja <andreja.prpic@gmail.com>
2019-08-26 23:09:36 +0200

    fix: fixed scoring
    Improved collision handling
    Fixed the position of rendered text.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
-const colWidth = 101;
-const rowHeight = 83;
-let gameOver = false;
-
 class Enemy {	
-	constructor(x, row, speed) {
-		this.x = x;
+	constructor(col, row, speed) {
+		this.colWidth = 101;
+		this.rowHeight = 83;
+		this.x = col * this.colWidth;
 		this.row = row;
 		this.sprite = 'images/enemy-bug.png';
 		this.collision = false;
@@ -18,19 +16,18 @@ class Enemy {
 		// all computers.
 		if(!this.collision) {
 			this.x += this.speed * dt;
-
-			if(this.x > 5 * colWidth) {
-				this.x = -1 * colWidth;
+			
+			if(this.x > 5 * this.colWidth) {
+				this.x = -1 * this.colWidth;
 				this.speed = 100 + (Math.random() * 300);
 			}
-
 			checkCollision(this);
 		}
 	}
 
 	// Draw the enemy on the screen, required method for game
 	render() {
-		ctx.drawImage(Resources.get(this.sprite), this.x, (this.row * rowHeight) - 20);
+		ctx.drawImage(Resources.get(this.sprite), this.x, (this.row * this.rowHeight) - 20);
 	}
 }
 
@@ -39,56 +36,78 @@ class Enemy {
 // a handleInput() method.
 class Player {
 	constructor(sprite = 'images/char-boy.png') {
-		this.col = 2;
-		this.row = 5;
 		this.rowHeight = 83;
 		this.colWidth = 101;
 		this.sprite = sprite;
-		this.collision = false;
-		this.score = 0;
-		this.lives = 5;
 		this.reset();
 	}
 
 	//when the player reaches the water it returns to its inital position
 	update() {
 		if(this.row === 0) {
-			this.score++;
-			setTimeout(() => this.resetPosition(), 1400);
+			if(!this.isScored) {
+				this.isScored = true;
+				this.score++;
+				setTimeout(() => this.resetPosition(), 1200);
+			}
 		}
 	}
 	
 	reset() {
-		this.curLives = this.lives;
-		this.score = 0;
 		this.resetPosition();
+		this.lives = 5;
+		this.score = 0;
+		this.gameOver = false;
 	}
 	
 	resetPosition() {
 		this.col = 2;
 		this.row = 5;
 		this.collision = false;
+		this.isScored = false; // to go back to normal
+	}
+	
+	handleCollision() {
+		this.collision = true;
+		this.lives--;
+		if(this.lives === 0) {
+			this.gameOver = true;
+		}
 	}
 	
 	render() {
 		this.renderLivesAndScore();
-		ctx.drawImage(Resources.get(this.sprite), this.col * colWidth, this.row * rowHeight - 10);
-		ctx.font = '30px Charcoal, sans-serif';
+		ctx.drawImage(Resources.get(this.sprite), this.col * this.colWidth, this.row * this.rowHeight - 10);
+		ctx.font = '30px Impact';
 		ctx.textAlign = 'center';
-		ctx.fillStyle = '#566ED7';
+		ctx.fillStyle = '#7ACD5D';
+		if(this.gameOver) {
+			ctx.fillStyle = 'red';
+			this.renderText('You\'re', -30);
+			this.renderText('dead!')
+			ctx.drawImage(Resources.get('images/bloodStain.png'), this.col * this.colWidth, this.row * this.rowHeight + 25);
+		} else if(this.collision) {
+			this.renderText('Ouch!');
+		} else if(this.row === 0) {
+			this.renderText('Hooray!');
+		}
+	}
+	
+	renderText(msg, yPos = 0) {
+		ctx.fillText(msg, (this.col + 0.5) * this.colWidth, (this.row * this.rowHeight) + 40 + yPos);
 	}
 	
 	renderLivesAndScore() {
-		ctx.font = '20px Charcoal';
+		ctx.font = '20px Impact';
 		ctx.textAlign = 'left';
 		ctx.fillStyle = 'white';
-		ctx.fillText('Lives: ' + this.curLives, 10, (7 * this.rowHeight));
+		ctx.fillText('Lives: ' + this.lives, 12, (7 * this.rowHeight) - 8);
 		ctx.textAlign = 'right';
-		ctx.fillText('Score: ' + this.score, (5 * this.colWidth ) - 10, (7 * this.rowHeight));
+		ctx.fillText('Score: ' + this.score, (5 * this.colWidth ) - 12, (7 * this.rowHeight) - 8);
 	}
 
 	handleInput(key) {
-		if(!this.collision) {
+		if(!this.collision && !this.gameOver && this.row !== 0) {
 			if(key === 'left' && this.col > 0) {
 				this.col --;
 			} else if (key === 'right' && this.col < 4) {
@@ -117,22 +136,22 @@ document.addEventListener('keyup', function(e) {
 
 const player = new Player('images/char-pink-girl.png');
 const allEnemies = [
-	new Enemy(-1 * colWidth, 1, 280),
-	new Enemy(-2 * colWidth, 2, 130),
-	new Enemy(-3 * colWidth, 3, 310)
+	new Enemy(-1, 1, 280),
+	new Enemy(-2, 2, 130),
+	new Enemy(-3, 3, 310)
 ];
 
 //handling collision
 function checkCollision(enemy) {
-	if(enemy.row === player.row && Math.abs(player.col * colWidth - enemy.x) <= 82) {
+	//82 = colWidth minus non-transparent parts of images
+	if(enemy.row === player.row && Math.abs(player.col * player.colWidth - enemy.x) <= 82) {
 		enemy.collision = true;
-		player.collision = true;
-		setTimeout(resetCollision, 1300);
+		player.handleCollision();
+		setTimeout(resetCollision, 1400);
 	}
 }
 
 function resetCollision() {
 	player.resetPosition();
-	player.curLives--;
 	allEnemies.forEach(enemy => enemy.collision = false);
 }
